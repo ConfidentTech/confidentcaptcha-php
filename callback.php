13cefebb2983b18194fa6d0398fa0bbe34e83fc7
John Whitlock <johnw@john-whitlocks-computer.local>
2010-07-14 14:10:28 -0500

    Remove old sample code

--- callback.php
+++ callback.php
@@ -1,66 +1,45 @@
 <?php 
 
-require_once("captchalib.php");
-require_once("config.php");
+/* Callback resource for Confident CAPTCHA AJAX calls
+ */
+
+require_once('config.php');
+require_once('confidentcaptcha/ccap_api.php');
+require_once('confidentcaptcha/ccap_persist.php');
+require_once('confidentcaptcha/ccap_policy_factory.php');
 
 session_start();
 
-function get_val($key, $default=null) {
-	if (isset($_REQUEST) and isset($_REQUEST[$key])) {
-		return $_REQUEST[$key];
-	} else if (isset($_SESSION) and isset($_SESSION['confidentcaptcha_'.$key])) {
-		return $_SESSION['confidentcaptcha_'.$key];
-	} else {
-		return $default;
-	}
-} 
+$ccap_api = new CCAP_API(
+    $ccap_api_settings['customer_id'],
+    $ccap_api_settings['site_id'],
+    $ccap_api_settings['api_username'],
+    $ccap_api_settings['api_password'],
+    $ccap_server_url);
+$ccap_persist = new CCAP_Persist_Session();
+$ccap_policy = CCAP_PolicyFactory::restore($ccap_persist, $ccap_api,
+    $ccap_default_policy);
 
-if ($_REQUEST['endpoint'] == 'block_onekey_start') {
-	$resp = start_block_onekey($_REQUEST['block_id'], $_REQUEST['phone_number'], $api_settings);
-	$_SESSION['confidentcaptcha_onekey_id'] = $resp['body'];
-	$xml = "<?xml version=\"1.0\"?>\n<response><status>".$resp['status']."</status><onekey_id>".$resp['body']."</onekey_id></response>";
-	header("Content-type: text/xml"); 
-	echo $xml;
+/* Generate callback response */
+function captcha_callback($ccap_policy)
+{
+    // Peform any setup needed at the start of a page w/ CAPTCHA
+    $start_error = $ccap_policy->start_captcha_page();
+    if ($start_error !== NULL) {
+        return "Error starting callback page: $start_error";
+    }
+    
+    $endpoint = (isset($_REQUEST['endpoint'])) ? $_REQUEST['endpoint'] : '';
+    return $ccap_policy->callback($endpoint, $_REQUEST);
 }
 
-elseif ($_REQUEST['endpoint'] == 'block_onekey_verify') {
-	$resp = check_block_onekey($_REQUEST['block_id'], $_REQUEST['captcha_id'], $api_settings);
-	$xml = simplexml_load_string($resp['body']);
-	if ($xml->authenticated == "True") {
-		$_SESSION['confidentcaptcha_onekey_verified'] = true;
-	}
-	header("Content-type: text/xml");
-	echo $resp['body'];
-}
-
-else if ($_REQUEST['endpoint'] == 'create_block') {
-	$block = create_block($api_settings, $_REQUEST['ip_addr'], $_REQUEST['user_agent']);
-	echo $block['body'];
-}
-
-else if ($_REQUEST['endpoint'] == 'create_captcha_instance') {
-	$display_style=get_val('display_style');
-	$include_audio=get_val('include_audio');
-	$height=get_val('height');
-	$width=get_val('width');
-	$captcha_length=get_val('captcha_length');
-	$resp = create_instance($_REQUEST['block_id'], $api_settings, $display_style,
-		$include_audio, $height, $width, $captcha_length);
-	if ($resp['status'] == 410) {
-		header($_SERVER["SERVER_PROTOCOL"]." 410 Gone");
-		exit;
-	}
-	echo $resp['body'];
-}
-
-else if ($_REQUEST['endpoint'] == 'verify_block_captcha') {
-	$resp = check_instance($_REQUEST['block_id'], $_REQUEST['captcha_id'],
-		$_REQUEST['code'], $api_settings);
-	if ($resp['status'] == 200) {
-		if ($resp['body'] == 'True') {
-			$_SESSION['confidentcaptcha_visual_verified'] = true;
-			echo 'true'; exit;
-		}
-	}
-	echo 'false'; exit;
+if ($ccap_policy) {    
+    $ret = captcha_callback($ccap_policy);
+    $content = $ret[0];
+    $headers = $ret[1];
+    foreach ($headers as $h) header($h);
+    echo $content;
+} else {
+    header("HTTP/1.0 500 Server Error");
+    echo 'Policy is not set, and $ccap_default_policy is invalid';
 }
